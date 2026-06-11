600c97d4143d1daacb960a679ff24ed896763ad3
vincentpham22 <126061203+vincentpham22@users.noreply.github.com>
2026-06-11 13:59:27 +0200

    update

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,13 +1,7 @@
-import { Box, Typography } from "@mui/material"
+import { Box } from "@mui/material"
 
 export default function Footer() {
     return (
-        <Box sx={{ display: "flex", alignItems: "center", justifyContent: "center", gap: 2, my: 9 }}>
-            <Box sx={{ width: {xs: "100px", sm: "200px", md:"400px"}, height: "1px", backgroundColor: "white", borderRadius: "2px" }} />
-            <Typography variant="h4">
-                FIN
-            </Typography>
-            <Box sx={{ width: {xs: "100px", sm: "200px", md:"400px"}, height: "1px", backgroundColor: "white", borderRadius: "2px" }} />
-        </Box>
+        <Box sx={{ my: 9 }} />
     )
 }
